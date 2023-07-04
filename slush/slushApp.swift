c7ff55b519e5c081b30e5c00a681df48a41d5301
kareemyusef <kareemyusefben@gmail.com>
2023-07-04 11:59:47 -0500

    Initial Commit

---
+++ slush/slushApp.swift
@@ -0,0 +1,17 @@
+//
+//  slushApp.swift
+//  slush
+//
+//  Created by Kareem Benaissa on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct slushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
