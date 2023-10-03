ec8f1e8cc7cd81d722527feccdd415b44f7de670
Aidan Lynde <130251073+aidanlynde@users.noreply.github.com>
2023-10-03 15:11:32 -0500

    SlushCreateView
    Assigns middle button in tabview to be call to action for SlushCreateView

--- slush/Views/Home/HomeView.swift
+++ slush/Views/Home/HomeView.swift
@@ -8,54 +8,62 @@ import SwiftUI
 
 struct HomeView: View {
     @ObservedObject var userViewModel: UserViewModel  // Inject the view model
-    @State private var selectedPage = 1  // Start with the middle page
+    @State private var selectedPage = 1  // Start with the HomeView as default
+    @State private var showSlushCreateView = false // Control the presentation of SlushCreateView
 
     var body: some View {
-        VStack {
-            // Main TabView
-            TabView(selection: $selectedPage) {
-                // Left Page - ListView
-                ListView(userViewModel: userViewModel)
-                    .tag(0)
-                // Middle Page - SlushHomeView
-                SlushHomeView(userViewModel: userViewModel)
-                    .tag(1)
-                // Right Page - MarketplaceView
-                MarketplaceView(userViewModel: userViewModel)
-                    .tag(2)
+        NavigationView {
+            VStack {
+                // Main TabView
+                TabView(selection: $selectedPage) {
+                    // First Page - ListView
+                    ListView(userViewModel: userViewModel)
+                        .tag(0)
+                    // Middle Page - SlushHomeView
+                    SlushHomeView(userViewModel: userViewModel)
+                        .tag(1)
+                    // Right Page - MarketplaceView
+                    MarketplaceView(userViewModel: userViewModel)
+                        .tag(2)
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))  // Hides the default page dots
+                .frame(maxHeight: .infinity) // Occupy all available space
+                
+                HStack {
+                    Button(action: {
+                        selectedPage = 0
+                    }) {
+                        Image(systemName: "list.bullet")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                    }
+                    Spacer()
+                    Button(action: {
+                        showSlushCreateView.toggle()  // Toggle the showSlushCreateView state
+                    }) {
+                        Image(systemName: "dollarsign.circle")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                    }
+                    Spacer()
+                    Button(action: {
+                        selectedPage = 2
+                    }) {
+                        Image(systemName: "house")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                    }
+                }
+                .padding()
             }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))  // Hides the default page dots
-            .frame(maxHeight: .infinity) // Occupy all available space
-            
-            HStack {
-                Button(action: {
-                    selectedPage = 0
-                }) {
-                    Image(systemName: "list.bullet") // List icon
-                        .resizable()
-                        .frame(width: 24, height: 24)
-                }
-                Spacer()
-                Button(action: {
-                    selectedPage = 1
-                }) {
-                    Image(systemName: "dollarsign.circle") // Dollar sign icon
-                        .resizable()
-                        .frame(width: 24, height: 24)
-                }
-                Spacer()
-                Button(action: {
-                    selectedPage = 2
-                }) {
-                    Image(systemName: "house") // House icon
-                        .resizable()
-                        .frame(width: 24, height: 24)
-                }
+            .sheet(isPresented: $showSlushCreateView) {
+                SlushCreateView()  // Present SlushCreateView as a sheet
             }
-            .padding()
         }
     }
 }
+
+
 
 
 
--- slush/Views/SlushView/SlushCreateView.swift
+++ slush/Views/SlushView/SlushCreateView.swift
@@ -5,4 +5,25 @@
 //  Created by Kareem Benaissa on 9/17/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct SlushCreateView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var body: some View {
+        VStack {
+            Text("Slush Creation Page")
+                .font(.largeTitle)
+                .padding()
+            
+            Button("Go back") {
+                presentationMode.wrappedValue.dismiss()
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+        }
+    }
+}
+
