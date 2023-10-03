7a135d2c89d9c412843d7085a0f9dec2272a8921
Aidan Lynde <130251073+aidanlynde@users.noreply.github.com>
2023-10-03 16:07:25 -0500

    Update HomeView
    added to do for fixing selecting ListView from Icon bug

--- slush/Views/Home/HomeView.swift
+++ slush/Views/Home/HomeView.swift
@@ -3,7 +3,9 @@
 //  slush
 //
 //  Created by Kareem Benaissa on 8/5/23.
-// View: Represents the UI. In SwiftUI, views are lightweight and often regenerated, so they should be free of app logic.
+// View: Represents the UI. In SwiftUI, views are lightweight and often regenerated, so they should be free of app logic.]
+
+
 import SwiftUI
 
 struct HomeView: View {
@@ -12,64 +14,79 @@ struct HomeView: View {
     @State private var showSlushCreateView = false
 
     var body: some View {
-        VStack {
+        ZStack {
             // This is the main content of your views, which takes up most of the screen.
+            
+            // TODO: Fix List bug to allow to click on the Icon after inital load
             TabView(selection: $selectedPage) {
                 ListView(userViewModel: userViewModel)
                 .tag(0)
                 
                 ScrollView {
-                    // Add your content here, for example:
+                    // Your content
                     Text("Slush Friends Activity")
-                    // ... other content
                 }
                 .tag(1)
-
 
                 MarketplaceView(userViewModel: userViewModel)
                 .tag(2)
             }
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             .frame(maxHeight: .infinity)
-
-            Divider()  // Add a line to visually separate the tab menu
-
-            // This is your fixed tab menu.
-            HStack {
-                Button(action: {
-                    selectedPage = 0
-                }) {
-                    Image(systemName: "list.bullet")
-                        .resizable()
-                        .frame(width: 21, height: 21)
-                        .foregroundColor(selectedPage == 0 ? Color.blue : Color.gray)
-                }
-                
-                Spacer().frame(width: 50)
-                
-                Button(action: {
-                    showSlushCreateView.toggle()
-                }) {
-                    Image(systemName: "dollarsign.circle")
-                        .resizable()
-                        .frame(width: 60, height: 60)
-                }
-                
-                Spacer().frame(width: 50)
-                
-                Button(action: {
-                    selectedPage = 2
-                }) {
-                    Image(systemName: "house")
-                        .resizable()
-                        .frame(width: 24, height: 24)
-                        .foregroundColor(selectedPage == 2 ? Color.blue : Color.gray)
+            .onChange(of: selectedPage) { _ in
+                // Force the view to update
+                withAnimation {
+                    self.selectedPage = selectedPage
                 }
             }
-            .padding()
+            
+            VStack {
+                Spacer()  // Push the menu to the bottom
+                
+                Divider()  // Add a line to visually separate the tab menu
+
+                // This is your fixed tab menu.
+                HStack {
+                    Button(action: {
+                        selectedPage = 0
+                    }) {
+                        Image(systemName: "list.bullet")
+                            .resizable()
+                            .frame(width: 21, height: 21)
+                            .foregroundColor(selectedPage == 0 ? Color.blue : Color.gray)
+                    }
+                    
+                    Spacer().frame(width: 50)
+                    
+                    Button(action: {
+                        showSlushCreateView.toggle()
+                    }) {
+                        Image(systemName: "dollarsign.circle")
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                    }
+                    .sheet(isPresented: $showSlushCreateView) {
+                        SlushCreateView()
+                    }
+                    
+                    Spacer().frame(width: 50)
+                    
+                    Button(action: {
+                        selectedPage = 2
+                    }) {
+                        Image(systemName: "house")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(selectedPage == 2 ? Color.blue : Color.gray)
+                    }
+                }
+                .padding()
+                .zIndex(1)  // This ensures the menu is above the TabView
+            }
         }
     }
 }
+
 
 
 
