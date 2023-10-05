7d64dd66de931cba5190a10d71c8da836eff3c9f
kareemyusef <kareemyusefben@gmail.com>
2023-10-05 14:34:33 -0500

    Adds symmetrical freinds relationship

--- slush/Services/FirestoreService.swift
+++ slush/Services/FirestoreService.swift
@@ -34,34 +34,51 @@ class FirestoreService {
 extension FirestoreService {
     
     func addFriend(uid: String, friendUID: String, completion: @escaping (Result<Void, Error>) -> Void) {
-            let userRef = db.collection("users").document(uid)
-            
-            // Add friendUID to the friends array
-            userRef.updateData([
-                "friends": FieldValue.arrayUnion([friendUID])
-            ]) { error in
-                if let error = error {
-                    completion(.failure(error))
-                } else {
-                    completion(.success(()))
-                }
+        let userRef = db.collection("users").document(uid)
+        let friendRef = db.collection("users").document(friendUID)
+        
+        // Begin a new write batch
+        let batch = db.batch()
+        
+        // Add friendUID to the friends array of uid
+        batch.updateData(["friends": FieldValue.arrayUnion([friendUID])], forDocument: userRef)
+        
+        // Add uid to the friends array of friendUID
+        batch.updateData(["friends": FieldValue.arrayUnion([uid])], forDocument: friendRef)
+        
+        // Commit the batch
+        batch.commit { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
             }
         }
+    }
 
-        func removeFriend(uid: String, friendUID: String, completion: @escaping (Result<Void, Error>) -> Void) {
-            let userRef = db.collection("users").document(uid)
-            
-            // Remove friendUID from the friends array
-            userRef.updateData([
-                "friends": FieldValue.arrayRemove([friendUID])
-            ]) { error in
-                if let error = error {
-                    completion(.failure(error))
-                } else {
-                    completion(.success(()))
-                }
+    func removeFriend(uid: String, friendUID: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        let userRef = db.collection("users").document(uid)
+        let friendRef = db.collection("users").document(friendUID)
+        
+        // Begin a new write batch
+        let batch = db.batch()
+        
+        // Remove friendUID from the friends array of uid
+        batch.updateData(["friends": FieldValue.arrayRemove([friendUID])], forDocument: userRef)
+        
+        // Remove uid from the friends array of friendUID
+        batch.updateData(["friends": FieldValue.arrayRemove([uid])], forDocument: friendRef)
+        
+        // Commit the batch
+        batch.commit { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
             }
         }
+    }
+
 }
 
 
