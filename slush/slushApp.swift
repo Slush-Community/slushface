7c73d838ff36cee99594dd01301f3b6ac3e0cab0
Aidan Lynde <130251073+aidanlynde@users.noreply.github.com>
2023-10-09 18:42:55 -0500

    Stylized LoginView
    Adds loading page prior to LoginView loading / adds styling to LoginView objects

--- slush/slushApp.swift
+++ slush/slushApp.swift
@@ -7,18 +7,58 @@
 
 import SwiftUI
 
+struct CircleProgress: View {
+    var progress: Double
+    var body: some View {
+        Circle()
+            .trim(from: 0.0, to: CGFloat(min(self.progress, 1.0)))
+            .stroke(Color.black, lineWidth: 5)
+            .rotationEffect(Angle(degrees: 270.0))
+    }
+}
+
 @main
 struct SlushApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    @StateObject private var userViewModel = UserViewModel() // Global ViewModel instance
+    @StateObject private var userViewModel = UserViewModel()
+    @State private var isLoading = true
+    @State private var progress = 0.0
 
     var body: some Scene {
         WindowGroup {
-            if userViewModel.isAuthenticated {
+            if isLoading {
+                VStack {
+                    ZStack {
+                        Image(systemName: "dollarsign.circle")
+                            .resizable()
+                            .frame(width: 120, height: 120)
+                            .foregroundColor(Color.green)
+                        
+                        CircleProgress(progress: progress)
+                            .frame(width: 120, height: 120)
+                    }
+                }
+                .onAppear {
+                    Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+                        if progress < 1.0 {
+                            withAnimation {
+                                progress += 0.05
+                            }
+                        } else {
+                            timer.invalidate()
+                            isLoading = false
+                        }
+                    }
+                }
+            } else if userViewModel.isAuthenticated {
                 HomeView(userViewModel: userViewModel)
+                    .transition(.opacity)
             } else {
                 LoginView(userViewModel: userViewModel)
             }
         }
     }
 }
+
+
+
